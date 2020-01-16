cf7dd397511f09859cfefc388ffcfc5a149c7506
Wilson Acosta <sayhi@wilsonacosta.us>
2020-01-16 01:04:53 -0600

    Setup App for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'loading...'
 
     const location = search.value;
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if( data.error ) {
             console.log(data.error)
