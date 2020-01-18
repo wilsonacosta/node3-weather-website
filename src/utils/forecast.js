67f62c1c0a32b35467f642d712971d6e05e66e4f
Wilson Acosta <sayhi@wilsonacosta.us>
2020-01-18 12:33:14 -0600

    Added temp high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if ( body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${Math.round(body.currently.precipProbability)}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${Math.round(body.currently.precipProbability)}% chance of rain. The Temperature will be a low of ${Math.round(body.daily.data[0].temperatureMin)} and a high of ${Math.round(body.daily.data[0].temperatureMax)}`)
         }
     })
 }
